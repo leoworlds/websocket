75baa409c8898ba3595b22a4c9cda77831ddac43
LManzhula <leoworlds@gmail.com>
2022-04-14 14:49:00 +0300

    first commit (add .gitignore)

---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+https://spring.io/guides/gs/messaging-stomp-websocket
+https://www.baeldung.com/java-websockets
+https://netbeans.apache.org/kb/docs/javaee/maven-websocketapi_ru.html
---
+++ src/main/java/main/WebSocketConfig.java
@@ -0,0 +1,61 @@
+package main;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
+import java.util.Map;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+
+        registry.addEndpoint("/gs-guide-websocket").setHandshakeHandler(new DefaultHandshakeHandler() {
+
+
+
+                    public boolean beforeHandshake(
+                            ServerHttpRequest request,
+                            ServerHttpResponse response,
+                            WebSocketHandler wsHandler,
+                            Map<String, Object> attributes) {
+
+                        if (request instanceof ServletServerHttpRequest) {
+                            ServletServerHttpRequest servletRequest
+                                    = (ServletServerHttpRequest) request;
+                            HttpSession session = servletRequest
+                                    .getServletRequest().getSession();
+                            attributes.put("sessionId", session.getId());
+                        }
+                        return true;
+                    }
+
+                    @Override
+                    protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+                        Principal user = super.determineUser(request, wsHandler, attributes);
+                        return user;
+                    }
+                })
+//                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+}
